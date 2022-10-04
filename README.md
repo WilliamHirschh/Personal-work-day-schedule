24ab1066b27eceb16f8fbed18b3842041f1a0948
William Hirsch <Williamhirschh@gmail.com>
2022-10-03 23:06:22 -0500

    fixed bugs in html and js

---
+++ README.md
@@ -0,0 +1,7 @@
+## Personal Work Day Schedule
+
+## Description
+    
+
+## Usage
+    The usage of a workday schedular is to keep track of upcoming events or activites throughout the day and store them to view later on
--- script.js
+++ script.js
@@ -12,27 +12,41 @@ $(document).ready(function(){
     update();
     setInterval(update, 1000);
 });
-
 var thingsToDo = {};
+const closeHr = 17;
+const openHr = 9;
+const BEFORE = 0;
 var opHours = 8;
 var opening = 9;
 var increment = 1;
 var closing = 17;
-const closeHr = 17;
-const openHr = 9;
-const BEFORE = 0;
 var backGround = BEFORE;
 const after = 1;
+
+function getOperatingHours(hours) {
+    var result;
+    if (hours < 12) {
+        result = hours.toString() + "AM";
+    }
+    else if (hours === 12 ){
+        result = hours.toString() + "PM";
+    }
+    else {
+        result = (hours-12).toString() + "PM";
+    }
+    return result;
+};
+
 function createHTML(hours){
     var stuff = $(
         "<div class='row'>" +
-        "<div class='column1 hour'>" + 
+        "<div class='col-1 hour'>" + 
             hours +
         "</div>" +
-        "<div class='column10'>" +
+        "<div class='col-10'>" +
             "<p class='description'></p>"+
         "</div>" +
-        "<div class='column1 saveBtn'>" +
+        "<div class='col-1 saveBtn'>" +
             "<i class = 'oi oi-task'></i>" +
         "</div>" +
     "</div>"
@@ -42,44 +56,28 @@ function createHTML(hours){
 };
 
 
-
-
-
-
-function convertTime(hours) {
-    var result;
-    if (hours < 12) {
-        result = hours.toString() + "AM";
-    }
-    else if (hours === 12) {
-        result = hours.toString() + "PM";
-    }
-    else {
-        result = (hours-12).toString() + "pm";
-    }
-return result;
-
-};
-function makeTaskSheet () {
-    for (opening; opening <= opening; opening++) {
-        createHTML(convertTime(opening));
+function makeTaskSheet (){
+    for (opening; opening <= closing; opening++) {
+        createHTML(getOperatingHours(opening));
     }
 };
 makeTaskSheet();
 
+
+
 function startCountDown (start) {
-    var AMPM = start.sliice(-2);
-    var num = start.slice(0, -2);
+    var AMPM = start.slice(-2);
+    var numm = start.slice(0, -2);
     var result;
 
     if (start === "12PM") {
         result = 12;
     }
     else if (AMPM === "PM") {
-        result = parseInt(num) + 12;
+        result = parseInt(numm) + 12;
     }
     else {
-        result = parseInt(num);
+        result = parseInt(numm);
     }
     return result;
 };
@@ -125,7 +123,7 @@ var load = function() {
         thingsToDo = {};
     }
     if (thingsToDo) {
-        $(".details").each(function( index ){
+        $(".description").each(function( index ){
             if(thingsToDo[index]) {
                 $( this ).text(thingsToDo[index]);
             }
@@ -133,18 +131,24 @@ var load = function() {
     }
 };
 load();
-$(".row").on("click", "column10", function(){
-var words = $(this).children(".details").text();
+
+$(".row").on("click", ".col-10", function() {
+    
+var words = $(this).children(".description").text();
+
 var wordsInput = $("<textarea>").addClass("form-control").val(words);
-$(this).children(".details").replaceWith(wordsInput);
+
+$(this).children(".description").replaceWith(wordsInput);
+
 wordsInput.trigger("focus");
 });
-$(".saveBtn").on("click", "i", function(){
-    var prevent = $(this).parent().prev().children(".form-control")
-    var savedText = prevent.val();
-    var savedIndex = prevent.closest(".row").index();
+$(".saveBtn").on("click", "i", function() {
+    var pick = $(this).parent().prev().children(".form-control")
+    var savedText = pick.val();
+    var savedIndex = pick.closest(".row").index();
     thingsToDo[savedIndex] = savedText;
     save();
     var paragraphTask = $("<p>").text(savedText);
-    prevent.closest(".form-control").replaceWith(paragraphTask);
+    pick.closest(".form-control").replaceWith(paragraphTask);
 });
+getTime();
